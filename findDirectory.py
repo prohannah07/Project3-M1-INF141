0be8c316b0773fa3bc21729ba34289234a745750
Hannah Fragante <hfragant@uci.edu>
2020-02-14 03:27:21 -0800

    fsadfs

--- findDirectory.py
+++ findDirectory.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     print(os.path.join(sys.argv[1], "0", "1"))
     print(os.path.isfile(os.path.join(sys.argv[1], "0", "1")))
 
-    soup = BeautifulSoup(os.path.join(sys.argv[1], "0", "1"), 'html.parser')
+    f = open(os.path.join(sys.argv[1], "0", "0"), 'r', encoding='utf8')
+
+    soup = BeautifulSoup(f, 'html.parser')
     print(soup.get_text())
     # corpus = Corpus(sys.argv[1])
