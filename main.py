050b83916ddc0633c3a9da7ae873b182711ce77b
MantaRey <reymunme@uci.edu>
2020-02-15 04:20:44 -0800

    Parsing File
    Unsure if completely correct. Does not seem to be "fixing" broken tags, but not sure how to make that work.

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import json
+import nltk
+import re
+import bs4
+import html5lib
 from bs4 import BeautifulSoup
+
+
+def parseElement(parent):
+    for child in parent.contents:
+        print("There is a CHILD Element of " + parent.name)
+        print(type(child))
+        if( isinstance(child, bs4.element.NavigableString) ):
+            print("This is a string: " + child)
+        if( isinstance(child, bs4.element.Tag) ):
+            print("Element Name: " + child.name)
+            parseElement(child)
 
 
 if __name__ == "__main__":
@@ -14,8 +29,10 @@ if __name__ == "__main__":
         URL = data[key]
         print("Folder: " + folderNum + "    File: " + fileNum + "   URL: " + URL)
         f = open("../WEBPAGES_CLEAN/" + folderNum + "/" + fileNum, 'r', encoding='utf8')
-        # print(f)
-        soup = BeautifulSoup(f, 'lxml-xml')
-        # print(soup.prettify())
-        for tag in soup.find_all(True):
-            print(tag.name)
+        # f = open("../WEBPAGES_CLEAN/" + "0" + "/" + "2", 'r', encoding='utf8')
+        soup = BeautifulSoup(f, 'html5lib')
+        # print(soup)
+        parseElement(soup)
+ 
+
+            
